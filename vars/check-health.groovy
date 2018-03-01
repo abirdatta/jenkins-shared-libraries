9ba07233e3c03e34fd96a805316aef0a00307b35
Abir Datta <abirdatta.in@gmail.com>
2018-03-01 17:42:34 +0530

    initial commit

--- vars/check-health.groovy
+++ vars/check-health.groovy
@@ -1,6 +1,6 @@
 def call(def server, def port) {
     retry (3) {
         sleep 5
-        httpRequest url:"http://${server}:${port}/health", validResponseCodes: '200', validResponseContent: '"status":"UP"'
+        httpRequest authentication: '7b15314c-558e-417d-8ba4-81787fd71b35', url:"http://${server}:${port}/health", validResponseCodes: '200', validResponseContent: '"status":"UP"'
     }
 }
